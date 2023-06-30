de69b72dbf8ddb6c0b3d451834d5d269d5279d00
Wendi Zhang <wendizhang68@gmail.com>
2023-06-30 16:23:15 -0400

    added custom hook for private route and useref

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,11 @@
 import {Navigate, Outlet} from 'react-router-dom'
+import {useAuthStatus} from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 const PrivateRoute = () => {
-  const loggedIn=false
+  const {loggedIn, loading} = useAuthStatus()
+  if(loading){
+   return <Spinner/>
+  }
   return loggedIn? <Outlet/>:<Navigate to='/signin'/>
   
 }
