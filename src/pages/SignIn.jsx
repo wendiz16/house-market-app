d6306f2c91136a783a96b98e1af4bfee3573e12b
Wendi Zhang <wendizhang68@gmail.com>
2023-07-07 17:14:18 -0400

    added google auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth'
 
 
 function SignIn() {
@@ -77,7 +78,7 @@ function SignIn() {
             </button>
           </div>
        </form>
-       {/* Google OAuth */}
+       <OAuth/>
 
        <Link to='/signup' className='registerLink'>
           Sign Up Instead
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 import {db} from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth';
 
 
 function SignUp() {
@@ -95,7 +96,7 @@ function SignUp() {
             </button>
           </div>
        </form>
-       {/* Google OAuth */}
+       <OAuth/>
 
        <Link to='/signin' className='registerLink'>
           Sign In Instead
