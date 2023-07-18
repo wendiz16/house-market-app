a8aeec2bcef0f43ca8e23b6d2c8d9fba1a64d6a2
Wendi Zhang <wendizhang68@gmail.com>
2023-07-18 15:52:34 -0400

    added createlisting

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
-
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -26,6 +26,7 @@ function App() {
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/create-listing' element={<CreateListing/>}/>
       </Routes>
       <Navbar/>
     </Router>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { useState} from 'react'
+import { Link } from 'react-router-dom'
 import { useNavigate} from 'react-router-dom'
 import { getAuth, updateProfile } from 'firebase/auth'
 import {updateDoc,doc} from 'firebase/firestore'
 import { db } from '../firebase.config'
 import {toast} from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 function Profile () {
   const auth = getAuth();
   const [changeDetails, setChangeDetails] = useState(false)
@@ -87,6 +90,12 @@ function Profile () {
             />
           </form>
         </div>  
+      
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='arrow right' />
+        </Link>
     </main>
   </div>
 }
